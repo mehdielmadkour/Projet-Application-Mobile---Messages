65280ee2c9de6a6fc5438693330f780f5087adb8
Mehdi El Madkour <mehdi.elmadkour@outlook.fr>
2021-06-21 14:50:38 +0200

    conversations data source

--- app/src/main/java/ca/uqac/programmationmobile/messages/ui/fragments/ConversationList.kt
+++ app/src/main/java/ca/uqac/programmationmobile/messages/ui/fragments/ConversationList.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import ca.uqac.programmationmobile.messages.R
 import ca.uqac.programmationmobile.messages.adapters.ConversationsAdapter
+import ca.uqac.programmationmobile.messages.data.ConversationsDataSource
 
 class ConversationList : Fragment() {
     override fun onCreateView(
@@ -19,6 +20,15 @@ class ConversationList : Fragment() {
         val view = inflater.inflate(R.layout.fragment_conversation_list, container, false)
 
         view.findViewById<RecyclerView>(R.id.conversation_list).adapter = ConversationsAdapter(null)
+
+        ConversationsDataSource().getUserConversations("").observe(viewLifecycleOwner, { holder ->
+            if (holder.conversations != null) {
+                (view.findViewById<RecyclerView>(R.id.conversation_list).adapter as ConversationsAdapter).updateData(holder.conversations)
+            }
+            else {
+                Toast.makeText(activity, holder.errorMsg, Toast.LENGTH_SHORT).show()
+            }
+        })
 
         return view
     }
