32e39919928afe69731f8d1007c917c4687bf8b2
Mehdi El Madkour <mehdi.elmadkour@outlook.fr>
2021-06-24 15:35:49 +0200

    send new messages

--- app/src/main/java/ca/uqac/programmationmobile/messages/data/MessagesDataSource.kt
+++ app/src/main/java/ca/uqac/programmationmobile/messages/data/MessagesDataSource.kt
@@ -9,6 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import ca.uqac.programmationmobile.messages.R
 import ca.uqac.programmationmobile.messages.adapters.ConversationsAdapter
 import ca.uqac.programmationmobile.messages.models.Message
+import ca.uqac.programmationmobile.messages.network.APIConversationInterface
+import ca.uqac.programmationmobile.messages.network.ApiClient
+import retrofit2.Call
+import retrofit2.Response
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -17,6 +21,12 @@ import java.util.*
 class MessagesDataSource(val context : Context, val viewLifecycleOwner: LifecycleOwner) {
 
     data class MessagesHolder(val messages: List<Message>?, val errorMsg: String?)
+
+    private var apiInterface: APIConversationInterface? = null
+
+    init {
+        apiInterface = ApiClient.getApiClient().create(APIConversationInterface::class.java)
+    }
 
     fun getMessagesfromConversation(conversationId: String) : LiveData<MessagesHolder> {
         val data = MutableLiveData<MessagesHolder>()
@@ -31,6 +41,13 @@ class MessagesDataSource(val context : Context, val viewLifecycleOwner: Lifecycl
         })
 
         return data
+    }
+
+    fun postMessage(conversationId: String, user: String, text: String) {
+        apiInterface?.postMessage(conversationId, user, text)?.enqueue(object : retrofit2.Callback<Unit>{
+            override fun onResponse(call: Call<Unit>, response: Response<Unit>) {}
+            override fun onFailure(call: Call<Unit>, t: Throwable) {}
+        })
     }
 
 }
